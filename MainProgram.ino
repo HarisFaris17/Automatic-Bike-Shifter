8f8ae3131d5a91388852e8a6607941666c52ec64
HarisFaris17 <harisfaris17@gmail.com>
2022-10-05 12:47:11 +0700

    fulfill the automatic gradient mode

--- MainProgram.ino
+++ MainProgram.ino
@@ -21,7 +21,7 @@
 #define UP 0x1
 #define DOWN 0x2
 
-const int gears[]={500,780,1050,1290,1500,1780,2000,2300};
+const int gears[] = { 500, 780, 1050, 1290, 1500, 1780, 2000, 2300, 2600};
 const int correctAmount = 200;
 const int startingGear = 2;
 const int maxGear = 9;
@@ -33,12 +33,13 @@ int ChangeDelayHeavy = 2000;  // recommended between 2500 and 5000
 int ChangeDelayLight = 4700;  // recommended between 2500 and 5000
 int MaxTurns = 5;             // recommended between 1 and 8
 int currentGear = 0;
+int previousFlooredGradient = 0;
 int action = NONE;
 int pos = 0;
 
 int mode = AUTOMATIC_GRADIENT;
 
-bool gearChanged = false;
+bool gearChanging = false;
 bool previousButtonMode = false;
 // bool buttonMode = false;
 bool senseModeChanges = false;
@@ -47,13 +48,13 @@ bool correctingGear = false;
 const unsigned long debounceInterval = 250;
 const unsigned long gearChangeInterval = 200;
 const unsigned long correctingGearInterval = 200;
-// const unsigned long pitchReadingInterval = 10;
+// const unsigned long gradientReadingInterval = 10;
 unsigned long timeModeChanges = 0;
 unsigned long timeHallEffectReadingBefore = 0;
 unsigned long timeGearChanged = 0;
 unsigned long timeCorrectingGear = 0;
 
-float pitch = 0.0f;
+float gradient = 0.0f;
 float pedalSpeed = 0.0f;
 
 // gyroscope
@@ -68,9 +69,9 @@ void setup() {
   pinMode(pinBicycleMode, INPUT_PULLUP);
   pinMode(hallEffectSensor, INPUT_PULLUP);
   // pinMode(pinBypass, INPUT_PULLUP);
-  pinMode(pinUpShift,INPUT_PULLUP);
-  pinMode(pinDownShift,INPUT_PULLUP);
-  attachInterrupt(hallEffectSensor,hallEffectReading, FALLING);
+  pinMode(pinUpShift, INPUT_PULLUP);
+  pinMode(pinDownShift, INPUT_PULLUP);
+  attachInterrupt(hallEffectSensor, hallEffectReading, FALLING);
   // attachInterrupt(pinBicycleMode, bicycleModeChanging, FALLING)
   Wire.begin();
   mpu.begin();
@@ -89,106 +90,114 @@ void checkMode() {
 
   // if there is changes in bicycle mode
   if (!senseModeChanges) {
-    if (buttonMode==LOW&&buttonMode!=previousButtonMode) {
-      if(mode==AUTOMATIC_GRADIENT) mode=AUTOMATIC_SPEED;
-      else if(mode==AUTOMATIC_SPEED) mode=MANUAL;
-      else if(mode==MANUAL) mode=AUTOMATIC_GRADIENT;
+    if (buttonMode == LOW && buttonMode != previousButtonMode) {
+      if (mode == AUTOMATIC_GRADIENT) mode = AUTOMATIC_SPEED;
+      else if (mode == AUTOMATIC_SPEED) mode = MANUAL;
+      else if (mode == MANUAL) mode = AUTOMATIC_GRADIENT;
       senseModeChanges = true;
       timeModeChanges = millis();
     }
   }
   // the mode in the next 250ms cant be changed to prevent bouncing, since before there is a change bicycle mode
   else {
-    if(millis()-timeModeChanges>debounceInterval){
+    if (millis() - timeModeChanges > debounceInterval) {
       senseModeChanges = false;
     }
   }
-  previousButtonMode=buttonMode;
+  previousButtonMode = buttonMode;
 }
 
-void bicycle(){
-  // if mode true, means it is automatic
-  if(mode==AUTOMATIC_GRADIENT){
-    automaticGradient();
-  }
-  // otherwise, it is manual
-  else if(mode==AUTOMATIC_SPEED){
-    automaticSpeed();
-  }
-  else if(mode==MANUAL){
-    manual();
+void bicycle() {
+  // if the gear is not changing
+  if (!gearChanging) {
+    // if mode true, means it is automatic
+    if (mode == AUTOMATIC_GRADIENT) {
+      automaticGradient();
+    }
+    // otherwise, it is manual
+    else if (mode == AUTOMATIC_SPEED) {
+      automaticSpeed();
+    } 
+    else if (mode == MANUAL) {
+      manual();
+    }
   }
 }
 
-void automaticGradient(){
+void automaticGradient() {
   mpu.update();
-  pitch=mpu.getAngleX();
-  
-  }
+  float gradient = mpu.getAngleX();
+  int targetGearByGradient = getGearByGradient((int)gradient);
+  int differenceGear = currentGear - targetGearByGradient;
+  if (differenceGradient > 1) action = DOWN; 
+  else if (differenceGear < -1) action = UP;
+  else if(differenceGear==0) action = NONE;
 }
 
-void automaticSpeed(){
+int getGearByGradient(int flooredGradient) {
+  return 9 - flooredGradient;
+}
+
+void automaticSpeed() {
 
 }
 
-void manual(){
-
+void manual() {
 }
 
-void changeGear(){
+void changeGear() {
   // there is a changes in gear before hence check if the delay time has exceeded
-  if(gearChanged){
-    if(!correctingGear){
-      if(millis()-timeGearChanged>gearChangeInterval){
-        timeCorrectingGear=millis();
-        correctingGear=true;
+  if (gearChanging) {
+    if (!correctingGear) {
+      if (millis() - timeGearChanged > gearChangeInterval) {
+        timeCorrectingGear = millis();
+        correctingGear = true;
       }
     }
     // the gear is not being corrected
-    else{
-      if(millis()-timeCorrectingGear>correctingGearInterval){
+    else {
+      if (millis() - timeCorrectingGear > correctingGearInterval) {
         servo.writeMicroseconds(pos);
-        gearChanged=false;
-        correctingGear=false;
+        gearChanging = false;
+        correctingGear = false;
         action = NONE;
       }
     }
   }
   // there are no changes in gears before or the delay changes in gears has exceeded, therefore allow changes in gear again
-  }else{
-    if(action==UP){
-      // if the currect gear is 9 or more, then do nothing
-      if(currentGear<maxGear){
-        currentGear++;
-        // remember that array starts from 0 and gears start from 1, hence to call the position pwm from specified gear, it should subtracted by 1
-        pos = gears[currentGear-1];
-        int extendedPos = pos + correctAmount;
-        servo.writeMicroseconds(extendedPos);
-        timeGearChanged=millis();
-        gearChanged=true;
-      }
+}
+else {
+  if (action == UP) {
+    // if the currect gear is 9 or more, then do nothing
+    if (currentGear < maxGear) {
+      currentGear++;
+      // remember that array starts from 0 and gears start from 1, hence to call the position pwm from specified gear, it should subtracted by 1
+      pos = gears[currentGear - 1];
+      int extendedPos = pos + correctAmount;
+      servo.writeMicroseconds(extendedPos);
+      timeGearChanged = millis();
+      gearChanging = true;
     }
-    else if (action==DOWN){
-      // if the currect gear is 1 or less, then do nothing
-      if(currentGear>1){
-        currentGear--;
-        pos = gears[currentGear-1];
-        int extendedPos = pos - correctAmount/4;
-        servo.writeMicroseconds(extendedPos);
-        timeGearChanged=millis();
-        gearChanged=true;
-      }
+  } else if (action == DOWN) {
+    // if the currect gear is 1 or less, then do nothing
+    if (currentGear > 1) {
+      currentGear--;
+      pos = gears[currentGear - 1];
+      int extendedPos = pos - correctAmount / 4;
+      servo.writeMicroseconds(extendedPos);
+      timeGearChanged = millis();
+      gearChanging = true;
     }
-    else if(action==NONE){
-      //do nothing
-    }
+  } else if (action == NONE) {
+    //do nothing
   }
 }
+}
 
-void resetSprocket(){
+void resetSprocket() {
   pos = gears[startingGear];
   // the position should extended to ensure the chain fits to the gear
-  int extendedPos = pos+correctAmount;
+  int extendedPos = pos + correctAmount;
   servo.writeMicroseconds(extendedPos);
   delay(200);
   servo.writeMicroseconds(pos);
@@ -197,9 +206,9 @@ void resetSprocket(){
 }
 
 // this function will be called when the there is a rising changes in hall effect sensor
-void hallEffectReading(){
+void hallEffectReading() {
   unsigned long timeHallEffectReadingNow = millis();
-  unsigned long intervalTwoHallEffectReading = timeHallEffectReadingNow-timeHallEffectReadingBefore;
-  pedalSpeed = (1.0/intervalTwoHallEffectReading)*KILO_HERTZ_TO_RPM;
+  unsigned long intervalTwoHallEffectReading = timeHallEffectReadingNow - timeHallEffectReadingBefore;
+  pedalSpeed = (1.0 / intervalTwoHallEffectReading) * KILO_HERTZ_TO_RPM;
   timeHallEffectReadingBefore = timeHallEffectReadingNow;
 }
